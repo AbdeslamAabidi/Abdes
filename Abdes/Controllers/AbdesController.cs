ac61812d22f5197924268f940c0febd1594e3246
abdeslam <abdeslam@nestmanagement.co.uk>
2018-10-11 15:42:41 +0100

    Added AbdesController and README.md

---
+++ Abdes/Controllers/AbdesController.cs
@@ -0,0 +1,32 @@
+﻿using Abdes.Data;
+using Abdes.Languages;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Abdes.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AbdesController : ControllerBase
+    {
+        // GET api/values/5
+        [HttpGet]
+        public dynamic Get(string language, string code)
+        {
+            var results = new Results();
+
+            switch (language.ToUpper())
+            {
+                case "CSHARP":
+                    results = (new CSharp()).Results(code);
+                    break;
+
+                default:
+                    results.Status = Enum.Status.error;
+                    results.Message = MessageResult.unkownlanguage;
+                    break;
+            }
+
+            return results;
+        }
+    }
+}
